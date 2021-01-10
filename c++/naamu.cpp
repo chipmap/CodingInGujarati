226926bca744c9ee08cb1458b8063e126f6074e6
Chirag Patel <chipmap@gmail.com>
2021-01-10 10:05:45 -0600

    Update naamu.cpp
    character array

--- c++/naamu.cpp
+++ c++/naamu.cpp
@@ -15,14 +15,37 @@ int main(int argc, char* argv[])
     sahaay();
 
     string naam = "";
+    char sarnamu[80] {0};
+    bool entryJamaa = true;
+    float rakam = 0;
 
     cout << "have khata mate naam aapo : ";
     getline(cin, naam);
     cout << "tame aapelu naam - " << naam << " - maate nondh" << endl;
-    cout << "kul akshar = " << naam.length() << endl;
-    cout << "biji rite akshar sankhya = " << naam.size() << endl;
 
-    cout << "thodik ramat " << "Shriman " + naam + " ji" << endl;
+    cout << "sarnamu? : \n";
+    int i = 0;
+    char c;
+    do
+    {
+        c = getchar();
+        sarnamu[i] = c;
+        i++;
+    } while ((c!='\n') || (i < 10));
+
+    cout << "jama/udhar chayan karo [1-jama, 0-udhaar] : ";
+    cin >> entryJamaa;
+
+    cout << "rakam ? : ";
+    cin >> rakam;
+
+    cout << "-------------------------" << endl;
+    cout << "Naam: " << naam << " (" << naam.length() << ")\n";
+    cout << "Sarnamu: " << sarnamu << endl;
+    cout << "ramat- " << sarnamu[0] << sarnamu[7] << endl;
+    string prakar = (entryJamaa == true) ? "jamaa" : "udhaar";
+    cout << "Prakar: " << prakar << endl;
+    cout << "Rakam: Rs" << rakam << endl;
 
     return 0;
 }
