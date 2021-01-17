7364f55583be72973b85b7e4d25cd77bc0cdb006
Chirag Patel <chipmap@gmail.com>
2021-01-17 11:30:36 -0600

    Added integer array example

--- c++/naamu.cpp
+++ c++/naamu.cpp
@@ -15,23 +15,41 @@ int main(int argc, char* argv[])
     sahaay();
 
     string naam = "";
-    char sarnamu[80] {0};
+    char sarnamu[80] = {0};
     bool entryJamaa = true;
     float rakam = 0;
 
+    // asthayi rupe shikhva mate j - prarambh
+
+    const int KUL_SANKHYA = 20;
+    int gun[KUL_SANKHYA] = {99, 80, 70};
+
+    gun[15] = 100;
+    gun[18] = 35;
+
+    cout << "-- kram -- gun --" << endl;
+    for (int kram = 0; kram <= (KUL_SANKHYA - 1); ++kram)
+    {
+        cout << "  " << kram + 1 << "    " << gun[kram] << endl;
+    }
+
+    // ant
+/*
     cout << "have khata mate naam aapo : ";
     getline(cin, naam);
     cout << "tame aapelu naam - " << naam << " - maate nondh" << endl;
 
     cout << "sarnamu? : \n";
-    int i = 0;
+    /*int i = 0;
     char c;
     do
     {
         c = getchar();
         sarnamu[i] = c;
         i++;
-    } while ((c!='\n') || (i < 10));
+    } while ((c != '\n') || (i < 80));
+    *
+    scanf("%79[^\n]", sarnamu);
 
     cout << "jama/udhar chayan karo [1-jama, 0-udhaar] : ";
     cin >> entryJamaa;
@@ -46,7 +64,7 @@ int main(int argc, char* argv[])
     string prakar = (entryJamaa == true) ? "jamaa" : "udhaar";
     cout << "Prakar: " << prakar << endl;
     cout << "Rakam: Rs" << rakam << endl;
-
+*/
     return 0;
 }
 
