74ac3e21ed1bdc677c7aefb3dce5aed37f6d9cc8
Chirag Patel <chipmap@gmail.com>
2021-02-07 10:13:14 -0600

    structure ange

--- c++/naamu.cpp
+++ c++/naamu.cpp
@@ -8,91 +8,113 @@
 
 using namespace std;
 
+const int ENTRY_NAAM_KUL_AKSHAR = 128;
+const int ENTRY_NONDH_KUL_AKSHAR = 128;
+const bool ENTRY_JAMAA = true;
+const bool ENTRY_UDHAAR = false;
+
+// ek entry mate joiti mahiti
+struct Entry
+{
+    // entry karti vela no samay
+    unsigned short varsh;
+    unsigned char mahino;
+    unsigned char divas;
+    unsigned char kalak;
+    unsigned char minute;
+    unsigned char second;
+    // kona mate aa levad-devad thai
+    //char naam[ENTRY_NAAM_KUL_AKSHAR];
+    string naam;
+    // rakam
+    float rakam;
+    // jama chhe ke udhar
+    bool jamaa;// true - jamaa, false udhaar
+    // entry mate nondh
+    //char nondh[ENTRY_NONDH_KUL_AKSHAR];
+    string nondh;
+};
+
 void sahaay(void);
+void entryNaakho(void);
+void ekEntryNaakho(void);
+void printEkEntry(Entry ekEntry);
 
 // prarambhik function
 int main(int argc, char* argv[])
 {
     sahaay();
-
-    string naam = "";
-    char sarnamu[80] = {0};
-    bool entryJamaa = true;
-    float rakam = 0;
-
-    // asthayi rupe shikhva mate j - prarambh
-
-    const int KUL_SANKHYA = 20;
-    int gun[KUL_SANKHYA] = {99, 80, 70};
-
-    gun[14] = 90;
-    gun[15] = 100;
-    gun[18] = 35;
-
-    cout << "-- kram -- gun --" << endl;
-    for (int kram = 0; kram <= (KUL_SANKHYA - 1); ++kram)
-    {
-        cout << "  " << kram + 1 << "    " << gun[kram] << endl;
-    }
-
-    int* mara_gun = nullptr;
-    mara_gun = &gun[14];
-    *mara_gun = 92;
-    ++mara_gun;
-    cout << "mara gun = addr:" << mara_gun
-            << " kimmat:" << *mara_gun
-            << " -- gun[14] = " << gun[14] << endl;
-
-    const int NAAM_AKSHAR = 16;
-    char* maru_naam = new char[NAAM_AKSHAR];
-    strcpy(maru_naam, "chirag");
-    cout << "naam = \'" << maru_naam << "\' (" << &maru_naam << ")" << endl;
-    delete[] maru_naam;
-
-    int* test = new int;
-    cout << "pahela test = " << test << " (" << &test << ")" << endl;
-    *test = 500;
-    cout << "pachhi test = " << *test << endl;
-    delete test;
-
-    // ant
-/*
-    cout << "have khata mate naam aapo : ";
-    getline(cin, naam);
-    cout << "tame aapelu naam - " << naam << " - maate nondh" << endl;
-
-    cout << "sarnamu? : \n";
-    /*int i = 0;
-    char c;
-    do
-    {
-        c = getchar();
-        sarnamu[i] = c;
-        i++;
-    } while ((c != '\n') || (i < 80));
-    *
-    scanf("%79[^\n]", sarnamu);
-
-    cout << "jama/udhar chayan karo [1-jama, 0-udhaar] : ";
-    cin >> entryJamaa;
-
-    cout << "rakam ? : ";
-    cin >> rakam;
-
-    cout << "-------------------------" << endl;
-    cout << "Naam: " << naam << " (" << naam.length() << ")\n";
-    cout << "Sarnamu: " << sarnamu << endl;
-    cout << "ramat- " << sarnamu[0] << sarnamu[7] << endl;
-    string prakar = (entryJamaa == true) ? "jamaa" : "udhaar";
-    cout << "Prakar: " << prakar << endl;
-    cout << "Rakam: Rs" << rakam << endl;
-*/
+    entryNaakho();
     return 0;
 }
 
+// karyakram ange vapraashkartaa ne janaavo
 void sahaay(void)
 {
     cout << "* * * * * * * * * * * * * * * * * * * *\n";
     cout << "*\t maaru naamu" << endl;
     cout << "* * * * * * * * * * * * * * * * * * * *\n";
+}
+
+// vaprashkarta pase thi entry ni mahiti lo
+void entryNaakho(void)
+{
+    char aavjo = 'n';
+
+    do
+    {
+        ekEntryNaakho();
+        cout << "naamu bandh karva y nu chayan karo... ";
+        cin >> aavjo;
+        cout << endl;
+    } while ((aavjo != 'y') && (aavjo != 'Y'));
+}
+
+void ekEntryNaakho(void)
+{
+    Entry ekEntry;
+
+    cout << "Entry maate nu varsh : ";
+    cin >> ekEntry.varsh;
+    cout << "Entry maate no mahino : ";
+    cin >> ekEntry.mahino;
+    cout << "Entry maate no divas : ";
+    cin >> ekEntry.divas;
+    cout << "Entry maate no samay [kalaak] : ";
+    cin >> ekEntry.kalak;
+    cout << "Entry maate no samay [minute] : ";
+    cin >> ekEntry.minute;
+    cout << "Entry maate no samay [second] : ";
+    cin >> ekEntry.second;
+
+    cout << "Levad devad thayeli rakam : ";
+    cin >> ekEntry.rakam;
+    cout << "Jamaa mate \'y\' ke \'Y\' athvaa udhaar ganaashe : ";
+    char jamaa;
+    cin >> jamaa;
+    if ((jamaa == 'y') || (jamaa = 'Y'))
+        ekEntry.jamaa = ENTRY_JAMAA;
+    else
+        ekEntry.jamaa = ENTRY_UDHAAR;
+
+    cout << "Entry kona maate kari enu naam : ";
+    cin >> ekEntry.naam;
+    cout << "Koi vishesh nondh? : ";
+    cin >> ekEntry.nondh;
+
+    printEkEntry(ekEntry);
+}
+
+// ek entry na printing maate
+void printEkEntry(Entry ekEntry)
+{
+    cout << "Me kareli entry --------------------" << endl;
+    cout << ekEntry.varsh << "/" << ekEntry.mahino << "/"
+        << ekEntry.divas << " "
+        << ekEntry.kalak << ":" << ekEntry.minute << ":"
+        << ekEntry.second
+        << " " << ekEntry.rakam
+        << " " << ((ekEntry.jamaa == ENTRY_JAMAA) ? "jamaa" : "udhaar")
+        << " " << ekEntry.naam << " " << ekEntry.nondh
+        << endl;
 }
