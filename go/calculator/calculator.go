77c97f0f09239e2bc7bcdf5aa41366889661b9f2
Chirag Patel <chipmap@gmail.com>
2021-09-26 11:24:46 -0500

    ફંક્શન અને પોઈન્ટર

--- go/calculator/calculator.go
+++ go/calculator/calculator.go
@@ -13,10 +13,26 @@ func sahaay() {
 	fmt.Println("કાર્યક્રમ બંધ કરવા q/Q નું ચયન કરો")
 }
 
+func beSankhyaLo(pr *float32, bi *float32) {
+	fmt.Print("પ્રથમ સંખ્યા લખો : ")
+	fmt.Scan(pr)
+	fmt.Print("બીજી સંખ્યા લખો : ")
+	fmt.Scan(bi)
+}
+
+func sarvalo(pr *float32, bi *float32) {
+	var parinam float32
+	parinam = *pr + *bi
+
+	fmt.Printf("%.2f + %.2f = %.2f\n", *pr, *bi, parinam)
+}
+
 func main() {
 	sahaay()
 
 	var nikalo bool = false
+	var pratham float32
+	var biji float32
 
 	for nikalo != true {
 		var vikalp string
@@ -24,9 +40,36 @@ func main() {
 		fmt.Scan(&vikalp)
 		fmt.Println("ચયન કરેલો વિકલ્પ : " + vikalp)
 
-		if strings.Compare(strings.ToLower(vikalp), "q") == 0 {
+		vikalp = strings.ToLower(vikalp)
+
+		switch vikalp {
+		case "q":
 			nikalo = true
+
+		case "+":
+			beSankhyaLo(&pratham, &biji)
+			sarvalo(&pratham, &biji)
+
+		/*case "-":
+			baadbaki()
+
+		case "x":
+			gunakar()
+
+		case "/":
+			bhagakar()
+
+		case "%":
+			takaa()
+
+		case "?":
+			shesh()*/
+
+		default:
+			fmt.Printf("ચયન કરેલો વિકલ્પ \"%s\" અજાણ્યો છે.\n", vikalp)
 		}
+
+		fmt.Print("નવા વિકલ્પનું ચયન કરો : ")
 	}
 
 	fmt.Println("કાર્યક્રમ કેવો રહ્યો એ અંગે પ્રતિભાવ આપશો! ધન્યવાદ!")
